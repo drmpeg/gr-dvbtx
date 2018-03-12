af264a5e8b9801810016f5e2c3a696ddc704f9d1
Ron Economos <w6rz@comcast.net>
2018-03-11 21:46:34 -0700

    Don't use __GR_VLA macro.

--- lib/dvbtx_ofdm_sym_acquisition_impl.cc
+++ lib/dvbtx_ofdm_sym_acquisition_impl.cc
@@ -104,8 +104,8 @@ namespace gr {
       int low, size;
 
       // Array to store peak positions
-      __GR_VLA(int, peak_pos, d_fft_length);
-      __GR_VLA(float, d_phi, d_fft_length);
+      int peak_pos[d_fft_length];
+      float d_phi[d_fft_length];
 
       // Calculate norm
       low = lookup_stop - (d_cp_length + d_fft_length - 1);
